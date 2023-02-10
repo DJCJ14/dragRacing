cd28bb56dbc1562221e9862201b7cbdb46dfca5e
DJCJ14 <dekfrek@gmail.com>
2023-02-10 08:06:52 -0600

    initCommit

--- dragRacing/GameViewController.swift
+++ dragRacing/GameViewController.swift
@@ -9,17 +9,74 @@ import UIKit
 import SpriteKit
 import GameplayKit
 
-class GameViewController: UIViewController {
 
+class GameViewController: UIViewController {
+    
+    @IBOutlet weak var raceButton: UIButton!
+    
+    @IBOutlet weak var acceleratorButton: UIButton!
+    
+    var counter = 0
+    var timer: Timer?
+    var play: GameScene!
+
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+//        if let view = self.view as! SKView? {
+//            // Load the SKScene from 'GameScene.sks'
+//            if let scene = SKScene(fileNamed: "GameScene") {
+//                // Set the scale mode to scale to fit the window
+//                scene.scaleMode = .aspectFill
+//
+//                // Present the scene
+//                view.presentScene(scene)
+//            }
+//
+//            view.ignoresSiblingOrder = true
+//
+//            view.showsFPS = true
+//            view.showsNodeCount = true
+//        }
+        
+        acceleratorButton.addTarget(self, action: #selector(buttonDown), for: .touchDown)
+        acceleratorButton.addTarget(self, action: #selector(buttonUp), for: [.touchUpInside, .touchUpOutside])
+        
+    }
+    
+    @objc func buttonDown(_ sender: UIButton) {
+        tinyAccel()
+        timer = Timer.scheduledTimer(timeInterval: 0.3, target: self, selector: #selector(accelerate), userInfo: nil, repeats: true)
+    }
+
+    @objc func buttonUp(_ sender: UIButton) {
+        timer?.invalidate()
+    }
+
+    func tinyAccel() {
+        play.accelerateCar()
+        print("tinyAccel")
+    }
+
+    @objc func accelerate() {
+        play.accelerateCar()
+        print(play.car.physicsBody?.velocity)
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return.landscapeRight
+    }
+    
+    @IBAction func raceAction(_ sender: UIButton) {
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
                 // Set the scale mode to scale to fit the window
                 scene.scaleMode = .aspectFill
-                
+                play = scene as?  GameScene
                 // Present the scene
                 view.presentScene(scene)
             }
@@ -29,17 +86,19 @@ class GameViewController: UIViewController {
             view.showsFPS = true
             view.showsNodeCount = true
         }
+        raceButton.isHidden = true
+        acceleratorButton.isHidden = false
     }
+    
 
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
+    
+    @IBAction func acceleratorAction(_ gestureRecognizer: UILongPressGestureRecognizer) {
 
-    override var prefersStatusBarHidden: Bool {
-        return true
+        
     }
+    
+    
+
 }
+
+
